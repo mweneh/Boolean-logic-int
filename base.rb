11f116271eb533f1c92a76a2dec1b5be8930efc6
David <davidongaroh@gmail.com>
2023-02-10 00:05:20 +0300

    use the eval method to evaluate

--- base.rb
+++ base.rb
@@ -10,4 +10,10 @@ class BooleanExpression
     expression.gsub!(/([a-z]+)/) do |match|
       VARIABLES[match] || match
     end
+    expression.gsub!("T", "true")
+    expression.gsub!("F", "false")
+    expression.gsub!("NOT", "!")
+    expression.gsub!("AND", "&&")
+    expression.gsub!("OR", "||")
+    eval(expression)
 end
