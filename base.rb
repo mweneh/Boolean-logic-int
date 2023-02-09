192f163f37e9fe73524bfa2ca1d18a35cddc2de8
David <davidongaroh@gmail.com>
2023-02-10 00:03:11 +0300

    evaluate expressions

--- base.rb
+++ base.rb
@@ -1,7 +1,13 @@
 class BooleanExpression
   VARIABLES = {}
-  
+
   def initialize(expression)
     @expression = expression
   end
+
+  def evaluate
+    expression = @expression.dup
+    expression.gsub!(/([a-z]+)/) do |match|
+      VARIABLES[match] || match
+    end
 end
